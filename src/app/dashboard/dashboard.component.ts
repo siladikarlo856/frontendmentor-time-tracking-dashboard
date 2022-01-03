5413f9de68a89d30cb00ba732de87a64cf27a1fb
Tester Siladi <testersiladi@gmail.com>
2022-01-03 23:11:13 +0100

    Functionality added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,7 +11,10 @@ import { ActivityService } from '../activity.service';
 })
 export class DashboardComponent implements OnInit {
   activityData: any = [];
+  regex = new RegExp("[ ]+", "g");
 
+  selectedTimeframe: string = "weekly";
+  timeframeLabel: string = "week";
 
   constructor(private activityService: ActivityService) {
   }
@@ -20,6 +23,24 @@ export class DashboardComponent implements OnInit {
     this.activityData = this.activityService.getActivityData();
 
     console.log(this.activityData);
+  }
+
+  onSelect(option: string): void {
+    console.log(option);
+    this.selectedTimeframe = option;
+
+    switch (this.selectedTimeframe) {
+      case "daily":
+        this.timeframeLabel = "day";
+        break;
+      case "weekly":
+        this.timeframeLabel = "week";
+        break;
+      case "monthly":
+        this.timeframeLabel = "month";
+        break;
+    }
+
   }
 
 }
