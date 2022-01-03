8d4de8b1cfd4f4ddf25579e5ed75ad3d36db5810
Tester Siladi <testersiladi@gmail.com>
2022-01-03 16:35:29 +0100

    UI initial verison

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-time-tracking-dashboard';
+  title = 'Frontend Mentor | Time tracking dashboard';
 }
